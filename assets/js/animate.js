79f4569539af5014b5fb4d424e3d21cf1f7913de
amit.sahu <amit.sahu@codenicely.in>
2024-06-14 04:47:33 +0530

    TF last Production release ::Fri Jun 14 04:47:33 IST 2024

--- assets/js/animate.js
+++ assets/js/animate.js
@@ -1,12 +1,24 @@
 /* observer for web page animations */
-const observer = new IntersectionObserver((entries) => {
+setTimeout(function () {
+  const observer = new IntersectionObserver((entries) => {
     entries.forEach((enrty) => {
-      console.log(enrty.isIntersecting);
+      console.log(enrty);
       if (enrty.isIntersecting) enrty.target.classList.add("animate-show");
-      else enrty.target.classList.remove("animate-show");
+      // else enrty.target.classList.remove("animate-show");
     });
   });
-  const hiddenItems = document.querySelectorAll(".animate-hidden");
-  hiddenItems.forEach((el) => observer.observe(el));
-  
-  /* end of observer*/
+  const ItemsSlideRight = document.querySelectorAll(".animate-slide-right");
+  ItemsSlideRight.forEach((el) => observer.observe(el));
+
+  const ItemsSlideLeft = document.querySelectorAll(".animate-slide-left");
+  ItemsSlideLeft.forEach((el) => observer.observe(el));
+
+  const ItemsSlideUp = document.querySelectorAll(".animate-slide-up");
+  ItemsSlideUp.forEach((el) => observer.observe(el));
+
+  const ItemsFillBgRight = document.querySelectorAll(".animate-fill-bg-right");
+  ItemsFillBgRight.forEach((el) => observer.observe(el));
+ 
+}, 250);
+
+/* end of observer*/
